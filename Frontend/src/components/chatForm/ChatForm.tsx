7ff35cd683dc9ea355acaf41fe815122c67282aa
bazlooka <kocicluka1@gmail.com>
2023-01-21 23:09:02 +0100

    Front ispeglan

--- Frontend/src/components/chatForm/ChatForm.tsx
+++ Frontend/src/components/chatForm/ChatForm.tsx
@@ -1,11 +1,50 @@
 import { TextField } from '@mui/material';
+import { Dispatch, FC, SetStateAction, useContext, useRef } from 'react';
+import { ChatMessage } from '../../models/chat-message';
+import UserContext from '../userManager/UserManager';
 
 import './ChatForm.css';
 
-const ChatForm = () => {
+interface Props {
+  setChatMessages: Dispatch<SetStateAction<ChatMessage[]>>;
+}
+
+const ChatForm: FC<Props> = ({ setChatMessages }) => {
+  const { user } = useContext(UserContext);
+
+  const textFieldRef = useRef<HTMLTextAreaElement>(null);
+
+  const onSendMessage = () => {
+    const msgText = textFieldRef.current?.value;
+    if (!user || !msgText) return;
+
+    const newMessage: ChatMessage = {
+      id: 'sd',
+      author: user,
+      text: msgText,
+      myMessage: true,
+      timeSent: new Date(),
+    };
+
+    setChatMessages((state) => [newMessage, ...state]);
+
+    textFieldRef.current.value = '';
+  };
+
   return (
     <div className="chat-form">
-      <TextField fullWidth placeholder="Napišite poruku" />
+      <TextField
+        size="small"
+        fullWidth
+        placeholder="Napišite poruku"
+        inputRef={textFieldRef}
+        onKeyPress={(ev) => {
+          if (ev.key === 'Enter') {
+            onSendMessage();
+            ev.preventDefault();
+          }
+        }}
+      />
     </div>
   );
 };
--- Frontend/src/components/chatMessageList/ChatMessageList.tsx
+++ Frontend/src/components/chatMessageList/ChatMessageList.tsx
@@ -1,78 +1,17 @@
+import { Dispatch, FC, SetStateAction } from 'react';
 import { ChatMessage } from '../../models/chat-message';
-import UserDto from '../../models/user.dto';
 import ChatMessageBubble from '../chatMessage/ChatMessageBubble';
 import './ChatMessageList.css';
 
-const ChatMessageList = () => {
-  const author: UserDto = {
-    id: 'kdfijgipfskdsok',
-    firstName: 'Luka',
-    lastName: 'Kocić',
-    username: 'luka',
-  };
+interface Props {
+  chatMessages: ChatMessage[];
+  setChatMessages: Dispatch<SetStateAction<ChatMessage[]>>;
+}
 
-  const messages: ChatMessage[] = [
-    {
-      id: '1',
-      author: author,
-      text: 'LALALALALALLALALAL',
-      timeSent: Date.now(),
-      myMessage: true,
-    },
-    {
-      id: '2',
-      author: author,
-      text: 'GBFSGFFDGBFBF',
-      timeSent: Date.now(),
-      myMessage: false,
-    },
-    {
-      id: '3',
-      author: author,
-      text: 'IJFIJIFDIFJF',
-      timeSent: Date.now(),
-      myMessage: false,
-    },
-    {
-      id: '4',
-      author: author,
-      text: 'JIFGJFIGFM',
-      timeSent: Date.now(),
-      myMessage: true,
-    },
-    {
-      id: '5',
-      author: author,
-      text: 'JGIFMIKMFIGM',
-      timeSent: Date.now(),
-      myMessage: false,
-    },
-    {
-      id: '6',
-      author: author,
-      text: 'IJFIJIFDIFJF',
-      timeSent: Date.now(),
-      myMessage: false,
-    },
-    {
-      id: '7',
-      author: author,
-      text: 'JIFGJFIGFM',
-      timeSent: Date.now(),
-      myMessage: true,
-    },
-    {
-      id: '8',
-      author: author,
-      text: 'JGIFMIKMFIGM DKFOKODFJK DFIPM DFM',
-      timeSent: Date.now(),
-      myMessage: false,
-    },
-  ];
-
+const ChatMessageList: FC<Props> = ({ chatMessages, setChatMessages }) => {
   return (
     <div className="chat-message-list">
-      {messages.map((m) => (
+      {chatMessages.map((m) => (
         <ChatMessageBubble key={m.id} message={m} />
       ))}
     </div>
